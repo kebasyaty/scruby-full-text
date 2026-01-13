fe5492f85cb9a25dcf201c6a67daef450800c07b
kebasyaty <kebasyaty@gmail.com>
2026-01-13 21:18:53 +0200

    update LANGUAGES

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -5,34 +5,34 @@ from __future__ import annotations
 from scruby_full_text import fullTextLang
 
 languages = {
-    "Arabic": "libstemmer_ar",
-    "Catalan": "libstemmer_ca",
-    "Chinese": "jieba_chinese",
-    "Czech": "stem_cz",
-    "Danish": "libstemmer_da",
-    "Dutch": "libstemmer_nl",
-    "English": "lemmatize_en_all",
-    "Finnish": "libstemmer_fi",
-    "French": "libstemmer_fr",
-    "German": "lemmatize_de_all",
-    "Greek": "libstemmer_el",
-    "Hindi": "libstemmer_hi",
-    "Hungarian": "libstemmer_hu",
-    "Indonesian": "libstemmer_id",
-    "Irish": "libstemmer_ga",
-    "Italian": "libstemmer_it",
-    "Japanese": "ngram_chars=japanese ngram_len=1",
-    "Korean": "ngram_chars=korean ngram_len=1",
-    "Lithuanian": "libstemmer_lt",
-    "Nepali": "libstemmer_ne",
-    "Norwegian": "libstemmer_no",
-    "Portuguese": "libstemmer_pt",
-    "Romanian": "libstemmer_ro",
-    "Russian": "lemmatize_ru_all",
-    "Spanish": "libstemmer_es",
-    "Swedish": "libstemmer_sv",
-    "Tamil": "libstemmer_ta",
-    "Turkish": "libstemmer_tr",
+    "Arabic": ("ar", "libstemmer_ar"),
+    "Catalan": ("ca", "libstemmer_ca"),
+    "Chinese": ("zh", "jieba_chinese"),
+    "Czech": ("cz", "stem_cz"),
+    "Danish": ("da", "libstemmer_da"),
+    "Dutch": ("nl", "libstemmer_nl"),
+    "English": ("en", "lemmatize_en_all"),
+    "Finnish": ("fi", "libstemmer_fi"),
+    "French": ("fr", "libstemmer_fr"),
+    "German": ("de", "lemmatize_de_all"),
+    "Greek": ("el", "libstemmer_el"),
+    "Hindi": ("hi", "libstemmer_hi"),
+    "Hungarian": ("hu", "libstemmer_hu"),
+    "Indonesian": ("id", "libstemmer_id"),
+    "Irish": ("ga", "libstemmer_ga"),
+    "Italian": ("it", "libstemmer_it"),
+    "Japanese": ("ja", "ngram_chars=japanese ngram_len=1"),
+    "Korean": ("ko", "ngram_chars=korean ngram_len=1"),
+    "Lithuanian": ("lt", "libstemmer_lt"),
+    "Nepali": ("ne", "libstemmer_ne"),
+    "Norwegian": ("no", "libstemmer_no"),
+    "Portuguese": ("pt", "libstemmer_pt"),
+    "Romanian": ("ro", "libstemmer_ro"),
+    "Russian": ("ru", "lemmatize_ru_all"),
+    "Spanish": ("es", "libstemmer_es"),
+    "Swedish": ("sv", "libstemmer_sv"),
+    "Tamil": ("ta", "libstemmer_ta"),
+    "Turkish": ("tr", "libstemmer_tr"),
 }
 
 
@@ -41,5 +41,8 @@ class TestPositive:
 
     def test_supported_languages(self) -> None:
         """Testing supported languages."""
-        for lang, morphology in languages.items():
-            assert fullTextLang.get(lang) == morphology
+        for lang, data in languages.items():
+            tmp = fullTextLang.get(lang)
+            assert tmp is not None
+            assert tmp[0] == data[0]
+            assert tmp[1] == data[1]
