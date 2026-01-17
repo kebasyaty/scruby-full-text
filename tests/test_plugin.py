2df058cc74c32adffc3f2eb12f1870144fb7a06f
kebasyaty <kebasyaty@gmail.com>
2026-01-17 12:27:08 +0200

    update test_plugin

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -8,6 +8,7 @@ from scruby import Scruby, ScrubyModel
 from scruby import settings as scruby_settings
 
 from scruby_full_text import FullText
+from scruby_full_text import settings as full_text_settings
 
 pytestmark = pytest.mark.asyncio(loop_scope="module")
 
@@ -37,5 +38,16 @@ class TestPositive:
 
     async def test_create_instance(self) -> None:
         """Create instance of plugin."""
-        # Get collection `Car`.
-        car_coll = await Scruby.collection(Car)  # noqa: F841
+        # Get collection `Car`
+        car_coll = await Scruby.collection(Car)
+        # Find a car
+        car: Car | None = await car_coll.plugins.fullText.find_one(
+            lang_morphology=full_text_settings.LANG_FULL_TEXT_SEARCH.get("English"),
+            full_text_filter={"brand": "???", "model": "???"},
+        )
+        #
+        assert car is None
+        #
+        # Full database deletion.
+        # Hint: The main purpose is tests.
+        Scruby.napalm()
