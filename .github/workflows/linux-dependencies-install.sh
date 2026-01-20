95b525f05c5b4774853f562aec52e395fbc5b9ad
kebasyaty <kebasyaty@gmail.com>
2026-01-20 23:24:07 +0200

    update .github/workflows/linux-dependencies-install.sh

--- .github/workflows/linux-dependencies-install.sh
+++ .github/workflows/linux-dependencies-install.sh
@@ -5,7 +5,7 @@ set -e
 apt update
 
 # Install OS dependencies
-apt install -y wget gnupg2 systemd git
+apt install -y wget gnupg2 systemctl git
 
 TEMP_DEB="$(mktemp)" &&
 wget -O "$TEMP_DEB" 'https://repo.manticoresearch.com/manticore-repo.noarch.deb' &&
